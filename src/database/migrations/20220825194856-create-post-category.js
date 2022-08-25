743e93bfbf3cd77559c03c82d13ea99138dc74f7
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-25 18:55:17 -0300

    fix/PostCategories: Added reference and cascade behavior to the postId and categoryId columns and made them not nullable.

--- src/database/migrations/20220825194856-create-post-category.js
+++ src/database/migrations/20220825194856-create-post-category.js
@@ -3,10 +3,20 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('PostCategories', {
       postId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'BlogPosts', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true
       },
       categoryId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Categories', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true
       }
     });
   },
