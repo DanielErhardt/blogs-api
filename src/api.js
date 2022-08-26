66b75a378af0a4f80dedc038761a471e474a861b
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-26 19:24:56 -0300

    feat/api.js: Created the necessary routes.

--- src/api.js
+++ src/api.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
-// ...
+const loginRouter = require('./routers/loginRouter');
+const userRouter = require('./routers/userRouter');
+const blogPostRouter = require('./routers/blogPostRouter');
+const categoryRouter = require('./routers/categoryRouter');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/login', loginRouter);
+
+app.use('/user', userRouter);
+
+app.use('/post', blogPostRouter);
+
+app.use('/categories', categoryRouter);
 // ...
 
 // É importante exportar a constante `app`,
