32316fbeb696e7e6eacd386bb03479b0afd7f612
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-25 21:48:19 -0300

    feat: Implemented BlogPost model properties and associations.

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,23 @@
+const BlogPost = (sequelize, DataTypes) => {
+  
+  const BlogPost = sequelize.define('BlogPost', {
+    id: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      as: 'user',
+      foreignKey: 'userId',
+    });
+
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
