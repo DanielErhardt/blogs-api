3ee37bbcbe802045d6fa5f0460ea6830dffa07d5
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-26 17:13:46 -0300

    feat: Created userService with all necessary, but empty methods.

---
+++ src/services/userService.js
@@ -0,0 +1,22 @@
+const { User } = require('../database/models');
+
+module.exports = {
+  login: async ({ email, password }) => {
+  },
+
+  create: async ({ email, password }) => {
+
+  },
+
+  findAll: async () => {
+
+  },
+
+  findByPk: async (id) => {
+
+  },
+
+  destroy: async (id) => {
+
+  },
+};
