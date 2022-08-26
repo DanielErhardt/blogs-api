9a8fae943506d3c136cfad4a82f8791a432d755e
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-26 17:14:47 -0300

    feat: Created userController with all necessary, but empty methods.

---
+++ src/controllers/userController.js
@@ -0,0 +1,22 @@
+const userService = require('../services/userService');
+
+module.exports = {
+  login: async (req, res) => {
+  },
+
+  create: async (req, res) => {
+
+  },
+
+  findAll: async (req, res) => {
+
+  },
+
+  findByPk: async (req, res) => {
+
+  },
+
+  destroyCurrentUser: async (req, res) => {
+
+  },
+};
