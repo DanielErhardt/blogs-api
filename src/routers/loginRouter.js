f026ea86aee92e56467a7f2037144d194220a904
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-27 11:01:14 -0300

    import/loginRouter: Imported and used express-rescue and login verificaton middleware in routes.

--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const rescue = require('express-rescue');
 const userController = require('../controllers/userController');
+const loginVerifier = require('../middlewares/loginVerifier');
 
-router.post('/', userController.login);
+router.post('/', loginVerifier, rescue(userController.login));
 
 module.exports = router;
