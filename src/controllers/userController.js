d0fae52d881e38ffd5795a8a18eb2527a74f33cc
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-27 11:06:18 -0300

    feat: Implemented database validations and response with token for /login endpoint.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,9 @@ const userService = require('../services/userService');
 
 module.exports = {
   login: async (req, res) => {
+    const { email, password } = req.body;
+    const token = await userService.login({ email, password });
+    return res.status(200).json({ token });
   },
 
   create: async (req, res) => {
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,17 @@
 const { User } = require('../database/models');
+const token = require('../utils/token');
+
+const RequestError = require('../utils/RequestError');
 
 module.exports = {
   login: async ({ email, password }) => {
+    const user = await User.findOne({ where: { email } });
+    
+    if (!user || user.password !== password) throw RequestError.invalidFields();
+
+    const newToken = token.create({ id: user.id });
+
+    return newToken;
   },
 
   create: async ({ email, password }) => {
