1ac2351c8238298815a5ba9f4a07244bfa752c1c
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-27 13:37:22 -0300

    feat/validations: Implemented schema for validating created user properties.

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 const RequestError = require('../utils/RequestError');
 
 const MISSING_FIELD = '400|Some required fields are missing';
+const STRING_MIN = '400|{#label} length must be at least {#limit} characters long';
 
 const loginSchema = Joi.object({
   email: Joi.string().email().required().messages({
@@ -12,8 +13,22 @@ const loginSchema = Joi.object({
   }),
 });
 
+const postUserSchema = Joi.object({
+  displayName: Joi.string().min(8).required().messages({
+    'string.min': STRING_MIN,
+  }),
+  email: Joi.string().email().required().messages({
+    'string.email': '400|{#label} must be a valid email',
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.min': STRING_MIN,
+  }),
+  image: Joi.string().required(),
+});
+
 module.exports = {
   loginSchema,
+  postUserSchema,
   convertToRequestError: (validationError) => {
     const { message } = validationError.details[0];
     return RequestError.fromCompositeMessage(message);
