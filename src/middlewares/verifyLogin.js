4e3da3ff1246816fc26212239e04d07663e923f8
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-27 15:05:42 -0300

    refactor/verifyLogin: Removed async from function declaration.

--- src/middlewares/verifyLogin.js
+++ src/middlewares/verifyLogin.js
@@ -1,6 +1,6 @@
 const { loginSchema, convertToRequestError } = require('./validations');
 
-module.exports = async (req, _res, next) => {
+module.exports = (req, _res, next) => {
   const { body: login } = req;
   const { error } = loginSchema.validate(login);
   
