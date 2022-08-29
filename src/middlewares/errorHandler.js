75de327ce108fa97b27f8bac2caa28f7a3090f8b
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-28 22:57:21 -0300

    lint: Added line breaks to response in errorHandler.

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,5 +2,7 @@ module.exports = async (error, _req, res, _next) => {
   const { statusCode, message, errors, parent, fields, original, sql, stack } = error;
   return statusCode
     ? res.status(statusCode).json({ message })
-    : res.status(500).json({ message: 'Run to the hills!', sql, errors, parent, fields, original, stack });
+    : res
+      .status(500)
+      .json({ message: 'Run to the hills!', sql, errors, parent, fields, original, stack });
 };
