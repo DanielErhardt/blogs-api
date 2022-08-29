61afeeab1e17f762700a350b4b991ebc6e471e99
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-28 22:39:28 -0300

    refactor/errorHandler: Exposed multiple properties in error response.

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 module.exports = async (error, _req, res, _next) => {
-  const { statusCode, message, stack } = error;
+  const { statusCode, message, errors, parent, fields, original, sql, stack } = error;
   return statusCode
     ? res.status(statusCode).json({ message })
-    : res.status(500).json({ message: 'Run to the hills!', stack });
+    : res.status(500).json({ message: 'Run to the hills!', sql, errors, parent, fields, original, stack });
 };
