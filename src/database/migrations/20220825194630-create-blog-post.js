8277d214136892563817719df77897af18432603
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-28 22:33:55 -0300

    refactor: Removed allowNull property from timestamps in create-blog-post migration.

--- src/database/migrations/20220825194630-create-blog-post.js
+++ src/database/migrations/20220825194630-create-blog-post.js
@@ -24,12 +24,10 @@ module.exports = {
         onDelete: 'CASCADE'
       },
       createdAt: {
-        allowNull: false,
         type: Sequelize.DATE,
         field:'published'
       },
       updatedAt: {
-        allowNull: false,
         type: Sequelize.DATE,
         field:'updated'
       }
